e439ffef0a7143458a1d6bba4b90f1dcf2db0c6b
provxpeuser <112288490+provxpeuser@users.noreply.github.com>
2026-01-11 13:24:07 -0500

    Add files via upload

---
+++ Script 6.lua
@@ -0,0 +1,8 @@
+getgenv().Config = {
+    Headless = true,
+
+    FakeDisplayName = "JebunnyNextDoor",
+    FakeName = "JebunnyNextDoor",
+    FakeId = 2945773814, -- input id
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/NameHider/main/MainScript.lua"))()
