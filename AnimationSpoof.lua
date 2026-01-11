b127d98cda36e0e90217f819006a16e8f83f5376
provxpeuser <112288490+provxpeuser@users.noreply.github.com>
2026-01-11 13:25:13 -0500

    Add custom animation settings and load animation script

---
+++ AnimationSpoof.lua
@@ -0,0 +1,8 @@
+getgenv().HybridSettings = {
+    run = "Ninja",
+    walk = "Ninja",
+    jump = "Levitate",
+}
+getgenv().EnableHybridCustom = true
+print("Custom Settings defined in getgenv().")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mautiku/Animation/refs/heads/main/Animation%20Changer%20v3%20Stable"))()
